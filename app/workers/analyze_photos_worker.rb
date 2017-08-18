ffae712cd2a99e1fa1e0d4ab48fd30f0d8ebb71e
Nick Prokesch <nprokesch@gmail.com>
2017-08-18 22:16:54 +0000

    Fix rate limiting based on driver

--- app/workers/analyze_photos_worker.rb
+++ app/workers/analyze_photos_worker.rb
@@ -1,39 +1,44 @@
 class AnalyzePhotosWorker
-  include Sidekiq::Worker
-  include Sidekiq::Throttled::Worker
-  sidekiq_options queue: :analysis
+    include Sidekiq::Worker
+    include Sidekiq::Throttled::Worker
+    sidekiq_options queue: :analysis
 
-  sidekiq_throttle({
-                     # Allow maximum 10 concurrent jobs of this class at a time.
-                     :concurrency => { :limit => 5 },
-                     # Allow maximum 1K jobs being processed within one hour window.
-                     :threshold => { :limit => 1_000, :period => 1.day }
-  })
-
-  def perform
-    photo = InstagramPhoto.where(scraped: [false,nil]).shuffle.sample
-    unless photo.nil?
-      client = ImageAnalysis::Client.new(url: photo.url, driver: driver)
-      if client.bad_image?
-        puts "Bad image", photo.url
-        # photo.destroy!
-      else
-        puts client.label_names.inspect, photo.url
-        photo.label_list.add(client.label_names)
-        if client.labels_include?('female', 'girl')
-          photo.gender = 'female'
-        end
-        photo.scraped = true
-        photo.save
-      end
+    if ENV['analysis_provider'] == 'rekognition'
+        sidekiq_throttle({
+            :concurrency => { :limit => 5 },
+            :threshold => { :limit => 1_000, :period => 1.day }
+        })
     else
-      DiscoveryWorker.perform_async
+        sidekiq_throttle({
+            :concurrency => { :limit => 10 },
+            :threshold => { :limit => 600, :period => 1.second }
+        })
     end
-  end
 
-  def driver
-    "ImageAnalysis::Drivers::#{ENV['analysis_provider'].camelcase}".constantize
-  end
+    def perform
+        photo = InstagramPhoto.where(scraped: [false,nil]).shuffle.sample
+        unless photo.nil?
+            client = ImageAnalysis::Client.new(url: photo.url, driver: driver)
+            if client.bad_image?
+                puts "Bad image", photo.url
+                # photo.destroy!
+            else
+                puts client.label_names.inspect, photo.url
+                photo.label_list.add(client.label_names)
+                if client.labels_include?('female', 'girl')
+                    photo.gender = 'female'
+                end
+                photo.scraped = true
+                photo.save
+            end
+        else
+            DiscoveryWorker.perform_async
+        end
+    end
+
+    def driver
+        "ImageAnalysis::Drivers::#{ENV['analysis_provider'].camelcase}".constantize
+    end
 end
 
 ProcessPhotosWorker = AnalyzePhotosWorker
