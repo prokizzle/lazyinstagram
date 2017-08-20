992b83abf1ceea4ec90976156d1478130d5b9592
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 17:38:52 +0000

    Add initial whitelist component

---
+++ app/javascript/packs/whitelist.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+
+
+class Whitelist extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            followedUsers: []
+        }
+    }
+
+    componentDidMount () {
+        this.fetchFollowedUsers()
+    }
+
+    fetchFollowedUsers () {
+        $.get('/followed-users', (users) => {
+            this.setState(followedUsers: users)
+        })
+    }
+
+    userList () {
+        return _.map(this.state.followedUsers, (user) => {
+            return <div className='card'>
+                <span><img src={user.profile_picture}/></span>
+                <span>{user.username}</span>
+            </div>
+        })
+    }
+
+    render () {
+        return <div>
+            <h1>Whitelist users</h1>
+
+            {this.userList.bind(this)()}
+        </div>
