da76b82e73c0eddc2611337acde6191d876df1f8
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 00:59:47 +0000

    Catch 404s from google vision api

--- app/models/image_analysis/drivers/google_vision.rb
+++ app/models/image_analysis/drivers/google_vision.rb
@@ -1,32 +1,38 @@
 require "google/cloud/vision"
 
 module ImageAnalysis
-  module Drivers
-    class GoogleVision < Base
-      attr_reader :labels
+    module Drivers
+        class GoogleVision < Base
+            attr_reader :labels
 
-      def initialize(url:)
-        project_id = ENV['google_vision_product_id']
-        client = Google::Cloud::Vision.new(project: project_id)
-        @labels = client.image(url).labels# rescue nil
-      end
+            def initialize(url:)
+                project_id = ENV['google_vision_product_id']
+                @client = Google::Cloud::Vision.new(project: project_id)
+                @labels = api_labels(url)
+            end
 
-      def label_names
-      	return [] if labels.nil?
-        labels.select do |label|
-          label.score > 0.5
-        end.map { |label| label.description.downcase }.uniq
-      end
+            def api_labels(url)
+                @client.image(url).labels
+            rescue Google::Cloud::InvalidArgumentError
+                nil
+            end
 
-      def bad_image?
-        labels.nil?
-      end
+            def label_names
+                return [] if labels.nil?
+                labels.select do |label|
+                    label.score > 0.5
+                end.map { |label| label.description.downcase }.uniq
+            end
 
-      def labels_include?(*labels)
-        labels.any? do |label|
-          label_names.include?(label)
+            def bad_image?
+                labels.nil?
+            end
+
+            def labels_include?(*labels)
+                labels.any? do |label|
+                    label_names.include?(label)
+                end
+            end
         end
-      end
     end
-  end
 end
