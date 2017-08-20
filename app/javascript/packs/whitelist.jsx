15d91c54956c5c6c685fadad9b50ad4e5cffae1a
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 19:05:18 +0000

    Fix order, add filter for whitelisted users

--- app/javascript/packs/whitelist.jsx
+++ app/javascript/packs/whitelist.jsx
@@ -8,7 +8,8 @@ class Whitelist extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            followedUsers: []
+            followedUsers: [],
+            whitelistedUsers: []
         }
     }
 
@@ -18,15 +19,31 @@ class Whitelist extends React.Component {
 
     fetchFollowedUsers () {
         $.get('/follows', (users) => {
-            this.setState({followedUsers: users})
+            this.setState({followedUsers: _.reverse(users)})
+        })
+    }
+
+    fetchWhitelistedUsers () {
+        $.get('/whitelists', (users) => {
+            this.setState({whitelistedUsers: users})
+        })
+    }
+
+    whitelistableUsers() {
+        return _.filter(this.state.followedUsers, (user) => {
+            return !this.state.whitelistedUsers.includes(user.id)
         })
     }
 
     userList () {
+
         return _.map(this.state.followedUsers, (user) => {
             return <div className='card'>
-                <span><img src={user.profile_picture}/></span>
-                <span>{user.username}</span>
+                <div className='row'>
+                    <span className='col-4'><img src={user.profile_picture}/></span>
+                    <span className='col-4'>{user.username}</span>
+                    <span className='col-4'><button onClick={handleAddToWhitelist} data-user-id={user.id}>Whitelist</button></span>
+                </div>
             </div>
         })
     }
