5244fee4f8ef7e4c4ec9ad100848d70380588500
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 00:00:59 +0000

    Put discovery in its own queue

--- app/workers/discovery_worker.rb
+++ app/workers/discovery_worker.rb
@@ -1,25 +1,26 @@
 class DiscoveryWorker
-  include Sidekiq::Worker
-  include Sidekiq::Throttled::Worker
-  
+    include Sidekiq::Worker
+    include Sidekiq::Throttled::Worker
+
+    sidekiq_options queue: :discovery
 
     sidekiq_throttle({
-                     # Allow maximum 10 concurrent jobs of this class at a time.
-                     :concurrency => { :limit => 5 },
-                     # Allow maximum 1K jobs being processed within one hour window.
-                     :threshold => { :limit => 1, :period => 1.minute }
-  })
+        # Allow maximum 10 concurrent jobs of this class at a time.
+        :concurrency => { :limit => 5 },
+        # Allow maximum 1K jobs being processed within one hour window.
+        :threshold => { :limit => 1, :period => 1.minute }
+    })
 
-  def perform
+    def perform
 
-    User.find_each do |user|
-      user.locations.each do |location|
-        ScrapePhotosByLocationWorker.perform_async(location.latitude, location.longitude, Time.now.to_i)
-      end
+        User.find_each do |user|
+            user.locations.each do |location|
+                ScrapePhotosByLocationWorker.perform_async(location.latitude, location.longitude, Time.now.to_i)
+            end
 
-      # user.hashtags.each do |hashtag|
-      # 	ScrapePhotosByHashtagWorker.perform_async(hashtag)
-      # end
+            # user.hashtags.each do |hashtag|
+            # 	ScrapePhotosByHashtagWorker.perform_async(hashtag)
+            # end
+        end
     end
-  end
 end
