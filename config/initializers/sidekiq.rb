c14ce1c61221d75a49a6debd0651af88af1c4927
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 04:56:38 +0000

    Use external redis server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,3 +6,11 @@ schedule_file = "config/schedule.yml"
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+Sidekiq.configure_server do |config|
+      config.redis = { url: 'redis://redis-10856.c11.us-east-1-2.ec2.cloud.redislabs.com:10856/1' }
+end
+
+Sidekiq.configure_client do |config|
+      config.redis = { url: 'redis://redis-10856.c11.us-east-1-2.ec2.cloud.redislabs.com:10856/1' }
+end
