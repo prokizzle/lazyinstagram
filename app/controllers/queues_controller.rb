6632431b2187da3db58f43109f407e51a9700526
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 05:47:26 +0000

    Add follow count, lower scrape concurrency

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -6,7 +6,8 @@ class QueuesController < ApplicationController
 			like_queue: female_photos.where(scraped: true, liked: false).size,
 			photos: InstagramPhoto.all.count,
 			liked: InstagramPhoto.where(liked: true).count,
-			most_popular_tags: ActsAsTaggableOn::Tag.most_used(15).map{|t| t.name}
+			most_popular_tags: ActsAsTaggableOn::Tag.most_used(15).map{|t| t.name},
+            followed: Follow.count,
 		}
 		render json: { queues: queues}
 	end
