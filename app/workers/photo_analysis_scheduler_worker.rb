564e37de99bb50b2adf48c89906c8daffd0fb806
Nick Prokesch <nprokesch@gmail.com>
2017-08-20 00:08:06 +0000

    Remove limit from analysis scheduler

--- app/workers/photo_analysis_scheduler_worker.rb
+++ app/workers/photo_analysis_scheduler_worker.rb
@@ -2,8 +2,6 @@ class PhotoAnalysisSchedulerWorker
   include Sidekiq::Worker
 
   def perform
-    if InstagramPhoto.where(liked: false, scraped: true, gender: 'female').count < 500
       InstagramPhoto.where(scraped: nil).count.times { AnalyzePhotosWorker.perform_async }
-    end
   end
 end
