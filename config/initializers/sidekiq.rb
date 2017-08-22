abe21d962afab3943dc83b20cc5b5672cc54f037
Nick Prokesch <nprokesch@gmail.com>
2017-08-22 21:49:40 +0000

    Fix syntax issue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,9 +9,9 @@ if File.exists?(schedule_file) && Sidekiq.server?
 end
 
 Sidekiq.configure_server do |config|
-      config.redis = { url: "redis://#{ENV[:REDIS_SERVER}:6379/0", password: ENV['REDIS_PASSWORD'] }
+      config.redis = { url: "redis://#{ENV['REDIS_SERVER']}:6379/0", password: ENV['REDIS_PASSWORD'] }
 end
 
 Sidekiq.configure_client do |config|
-      config.redis = { url: "redis://#{ENV[:REDIS_SERVER}:6379/0", password: ENV['REDIS_PASSWORD'] }
+      config.redis = { url: "redis://#{ENV['REDIS_SERVER']}:6379/0", password: ENV['REDIS_PASSWORD'] }
 end
