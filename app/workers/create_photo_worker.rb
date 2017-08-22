a7140f2d85dd4391a953f6771a4614b6de45bde0
Nick Prokesch <nprokesch@gmail.com>
2017-08-22 19:04:46 +0000

    Split create photo worker into its own worker

---
+++ app/workers/create_photo_worker.rb
@@ -0,0 +1,14 @@
+class CreatePhotoWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: :creations
+
+    def perform(url, photo_id, user_id, tags)
+        photo = InstagramPhoto.find_or_create_by(
+            url: result['images']['standard_resolution']['url'],
+            photo_id: result['id'],
+            user_id: result['user']['id']
+        )
+        photo.hashtag_list.add(result['tags'])
+        photo.save
+    end
+
