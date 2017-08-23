7b7a10e1bc6a80de03835291c19286c198ff9712
Nick Prokesch <nprokesch@gmail.com>
2017-08-23 00:22:40 +0000

    Fix cron loading

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,16 +2,39 @@ require "sidekiq/throttled"
 require 'sidekiq-cron'
 Sidekiq::Throttled.setup! 
 
-schedule_file = "config/schedule.yml"
+array = [
+    {
+        "name": "process_photos",
+        "cron": "* * * * *",
+        "class": "PhotoAnalysisSchedulerWorker",
+        "queue": "analysis"
+    },
+    {
+    "name": "discovery",
+        "cron": "*/5 * * * *",
+        "class": "DiscoveryWorker",
+        "queue": "discovery"
+    },
+    {
+        "name": "like_photos",
+        "cron": "*/1 * * * *",
+        "class": "PhotoLikerWorker",
+        "queue": "instagram_api"
+    },
+    {
+        "name": "follow_users",
+        "cron": "*/60 * * * *",
+        "class": "ScheduleFollowsWorker",
+        "queue": "follows"
+    }
+]
 
-if File.exists?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
+Sidekiq::Cron::Job.load_from_array array
 
 Sidekiq.configure_server do |config|
-      config.redis = { url: "redis://#{ENV['REDIS_SERVER']}:6379/0", password: ENV['REDIS_PASSWORD'] }
+    config.redis = { url: "redis://#{ENV['REDIS_SERVER']}:6379/0", password: ENV['REDIS_PASSWORD'] }
 end
 
 Sidekiq.configure_client do |config|
-      config.redis = { url: "redis://#{ENV['REDIS_SERVER']}:6379/0", password: ENV['REDIS_PASSWORD'] }
+    config.redis = { url: "redis://#{ENV['REDIS_SERVER']}:6379/0", password: ENV['REDIS_PASSWORD'] }
 end
