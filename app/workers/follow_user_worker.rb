029c235a47305192cb500168415f75cf67a04863
Nick Prokesch <nprokesch@gmail.com>
2017-09-09 04:04:32 +0000

    Halt following at 400 follows

--- app/workers/follow_user_worker.rb
+++ app/workers/follow_user_worker.rb
@@ -10,7 +10,7 @@ class FollowUserWorker
     sidekiq_options queue: :follows, unique: :until_executed
 
     def perform(user_id)
-        return if Instagram::Account.new.total_following > 500
+        return if Instagram::Account.new.total_following > 400
         follow = Follow.find_or_create_by(user_id: user_id)
         return if follow.following? || (follow.followed_at && follow.followed_at < 1.month.ago)
         user_follower = Instagram::UserFollower.new(user_id: user_id)
