98cac037e20c0417b847cc635de975dd5b1eaa5b
Nick Prokesch <nprokesch@gmail.com>
2017-09-09 04:02:52 +0000

    Update throttling

--- app/workers/create_photo_worker.rb
+++ app/workers/create_photo_worker.rb
@@ -1,6 +1,6 @@
 class CreatePhotoWorker
     include Sidekiq::Worker
-    sidekiq_options({queue: :creations, concurrency: 6})
+    sidekiq_options({queue: :creations})
 
     def perform(url, photo_id, user_id, tags)
         photo = InstagramPhoto.find_or_create_by(
