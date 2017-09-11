9a8695367eebe17f6c7539c12a00bc1b31e7078b
Nick Prokesch <nprokesch@gmail.com>
2017-09-11 02:32:05 +0000

    Abstract unfollow queue into utlity class

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -6,6 +6,7 @@ class QueuesController < ApplicationController
 			like_queue: female_photos.where(scraped: true, liked: false).size,
 			photos: InstagramPhoto.all.count,
 			liked: InstagramPhoto.where(liked: true).count,
+            unfollow_queue: Instagram::UserUnfollower.queue.count,
             followed: Follow.count,
 		}
 		render json: { queues: queues}
--- app/workers/unfollow_user_worker.rb
+++ app/workers/unfollow_user_worker.rb
@@ -10,11 +10,7 @@ class UnfollowUserWorker
     sidekiq_options({ backtrace: true, unique: :until_executing })
 
     def perform
-        following_ids = Instagram::Account.new.following_ids
-        whitelisted_ids = Whitelist.where(instagram_user_id: following_ids).pluck(:instagram_user_id)
-        targets = Follow.where.not(user_id: whitelisted_ids).where(followed_at: 1.year.ago..3.days.ago, following: true).order(followed_at: :desc)
-        puts targets.count
-        target = targets.try(:first)
+        target = Instagram::UserUnfollower.queue.try(:first)
         return if target.nil?
         client = Instagram::UserUnfollower.new(user_id: target.user_id)
         client.unfollow!
