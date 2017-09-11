a84ae9098fd9ab7a020a87515311619558d63aff
Nick Prokesch <nprokesch@gmail.com>
2017-09-11 02:18:07 +0000

    Reduce queues

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -1,12 +1,11 @@
 class QueuesController < ApplicationController
 	def index
-		female_photos = InstagramPhoto.tagged_with(['girl', 'female'], any: true, on: :labels).tagged_with('advertising', exclude: true)
+        female_photos = InstagramPhoto.females
 		queues = { 
 			analysis_queue: InstagramPhoto.where(scraped: nil).size,
 			like_queue: female_photos.where(scraped: true, liked: false).size,
 			photos: InstagramPhoto.all.count,
 			liked: InstagramPhoto.where(liked: true).count,
-			most_popular_tags: ActsAsTaggableOn::Tag.most_used(15).map{|t| t.name},
             followed: Follow.count,
 		}
 		render json: { queues: queues}
