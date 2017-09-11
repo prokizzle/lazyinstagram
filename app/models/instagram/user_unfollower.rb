a249a0c3db8369ba787635ea1dd5c9e30b22d747
Nick Prokesch <nprokesch@gmail.com>
2017-09-11 02:32:05 +0000

    Abstract unfollow queue into utlity class

--- app/models/instagram/user_unfollower.rb
+++ app/models/instagram/user_unfollower.rb
@@ -8,5 +8,13 @@ module Instagram
             return false if results['meta']['error_type'].present?
             results['data']['outgoing_status'] == 'none'
         end
+
+        def self.queue
+            following_ids = Instagram::Account.new.following_ids
+            whitelisted_ids = Whitelist.where(instagram_user_id: following_ids).pluck(:instagram_user_id)
+            Follow.where.not(
+                user_id: whitelisted_ids
+            ).where(followed_at: 1.year.ago..3.days.ago, following: true).order(followed_at: :desc)
+        end
     end
 end
